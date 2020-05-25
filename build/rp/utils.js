ddc974786df4c54f9b377808033b8bc0d64910ae
Benjamin Flin <brf2117@columbia.edu>
2020-05-25 15:15:44 -0700

    stat and exec partial implementation

--- build/rp/utils.js
+++ build/rp/utils.js
@@ -1,21 +1,16 @@
-
-utils=require("rputils");
-i=0
+var utils = require("./libdukutils.so");
+var i = 0;
 
 try {
-
-utils.readln("./server.js",function(line){
-    print(line);
-    i++
-    if(i>55) return false;
-});
-
-} catch(e) {
-    print("caught:");
-    console.log(e);
+  utils.readln("./curltest2.js", function (line) {
+    // print(line);
+    i++;
+    if (i > 55) return false;
+  });
+  var stat = utils.stat("./curltest2.js");
+  print(stat.last_access);
+  print(utils.exec("/bin/ls", "ls", "-1"));
+} catch (e) {
+  print("caught:");
+  console.log(e);
 }
-
-
-//utils.readcsv("test2.csv",function(){
-//    print("in function");
-//});
--- rp/utils.c
+++ rp/utils.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
-#include <stdarg.h>  /* va_list etc */
-#include <stddef.h>  /* size_t */
+#include <stdarg.h> /* va_list etc */
+#include <stddef.h> /* size_t */
 #include <limits.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include <inttypes.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
 #include "duktape.h"
+#define REMALLOC(s, t)               \
+  do                                 \
+  {                                  \
+    (s) = realloc((s), (t));         \
+    if ((char *)(s) == (char *)NULL) \
+    {                                \
+      return (2000001);              \
+    }                                \
+  } while (0)
 
-#define REMALLOC(s,t)  do {(s) = realloc( (s) , (t) ); if ((char*)(s)==(char*)NULL){ return(2000001); } } while(0)
 #define BUFREADSZ 4096
-#define MAXBUFFER 536870912  /* 512mb */
+#define MAXBUFFER 536870912 /* 512mb */
 #define GETDATAEND 1999999
 
 /* fill buffer with more data from file.
@@ -30,19 +42,19 @@
 
 int getdata(FILE *fd, char **buf, char **curend, size_t *cursize, int wrappos)
 {
-  int i=0,curdatasize=0;
+  int i = 0, curdatasize = 0;
   char *b;
-
-  errno=0;
+  errno = 0;
   /* get a block of memory, 
      curend shouldn't be set in this case*/
   if (*cursize < BUFREADSZ)
   {
-    *cursize=BUFREADSZ;
-    REMALLOC(*buf,*cursize+1);
+    *cursize = BUFREADSZ;
+    REMALLOC(*buf, *cursize + 1);
   }
   /* set end of buffer data if not set */
-  if (*curend==(char*)NULL) *curend=*buf+BUFREADSZ; /* -1 == end of buffer */
+  if (*curend == (char *)NULL)
+    *curend = *buf + BUFREADSZ; /* -1 == end of buffer */
 
   /* check if we are wrapping.
      If so, move data from wrap point to curend to beginning of buffer.
@@ -51,175 +63,346 @@ int getdata(FILE *fd, char **buf, char **curend, size_t *cursize, int wrappos)
   if (wrappos > -1)
   {
     /* get size of data to copy to beginning of string */
-    curdatasize = *curend - (*buf + wrappos) ;
-//printf("curdatasize(%d)=curend(%d) - (buf(%d) + wrappos(%d)\n", curdatasize,(int)*curend,(int)*buf,wrappos);
-//printf("curend-1='%c', buf+wrappos='%c'\n",*(*curend-1),*(*buf+wrappos));
-    int oldsize=*cursize;
+    curdatasize = *curend - (*buf + wrappos);
+    //printf("curdatasize(%d)=curend(%d) - (buf(%d) + wrappos(%d)\n", curdatasize,(int)*curend,(int)*buf,wrappos);
+    //printf("curend-1='%c', buf+wrappos='%c'\n",*(*curend-1),*(*buf+wrappos));
+    int oldsize = *cursize;
     /* is our buffer currently large enough for new block of data ? */
-    if( curdatasize + BUFREADSZ > *cursize)
+    if (curdatasize + BUFREADSZ > *cursize)
     {
-      *cursize+=BUFREADSZ;
+      *cursize += BUFREADSZ;
       if (*cursize > MAXBUFFER)
         return (2000000); /* so as not to be confused with errno range errors*/
-      REMALLOC(*buf,*cursize+1);
+      REMALLOC(*buf, *cursize + 1);
     }
-    if(wrappos) /* "Wherever you go, there you are" --Buckaroo Banzai 
+    if (wrappos) /* "Wherever you go, there you are" --Buckaroo Banzai 
                    "If you don't know where you are going, you'll end up someplace else." -- Yogi Berra. */
       /* move data that starts at wrappoint to beginning of buf */
-      memmove(*buf, *buf+wrappos, curdatasize);
+      memmove(*buf, *buf + wrappos, curdatasize);
     /* else if wrappos is 0, don't memmove from 0 to 0 */
 
-    b=*buf+curdatasize;
+    b = *buf + curdatasize;
   }
   /* no wrapping, overwrite buffer */
-  else 
+  else
   {
-    b=*buf;
+    b = *buf;
   }
-  i=(int)fread(b, 1, BUFREADSZ , fd );
-  *curend=b+i;
+  i = (int)fread(b, 1, BUFREADSZ, fd);
+  *curend = b + i;
 
-  *(*curend)='\0'; /* at 4097 */
+  *(*curend) = '\0'; /* at 4097 */
 
-//printf("i=%d\n",i);
-  if (i<BUFREADSZ)
-    return(GETDATAEND);
+  //printf("i=%d\n",i);
+  if (i < BUFREADSZ)
+    return (GETDATAEND);
 
-  return(errno);  
+  return (errno);
 }
 
-FILE *openFileWithCallback(duk_context *ctx, int *func_idx, const char **fn)
+FILE *openFileWithCallback(duk_context *ctx, int *func_idx, const char **filename)
 {
-  FILE * fp;
-  int i=0;
-  
-  if(duk_is_string(ctx,i) || duk_is_string(ctx,++i) )
-    *fn=duk_get_string(ctx,i);
+  int i = 0;
+
+  if (duk_is_string(ctx, i) || duk_is_string(ctx, ++i))
+    *filename = duk_get_string(ctx, i);
   else
   {
-    duk_push_string(ctx,"readln requires a file name passed as a string");
-    (void) duk_throw(ctx);
+    duk_push_string(ctx, "readln requires a file name passed as a string");
+    (void)duk_throw(ctx);
   }
-  
-  i=!i; /* get the other option */
-  
-  if(duk_is_function(ctx,i))
-    *func_idx=i;
+
+  i = !i; /* get the other option */
+
+  if (duk_is_function(ctx, i))
+    *func_idx = i;
   else
   {
-    duk_push_string(ctx,"readln requires a callback function");
-    (void) duk_throw(ctx);
+    duk_push_string(ctx, "readln requires a callback function");
+    (void)duk_throw(ctx);
   }
-  /* TODO: check for directory */
-  return(fopen(*fn, "r"));
+
+  struct stat path_stat;
+  int err = stat(*filename, &path_stat);
+  if (err)
+  {
+    duk_push_sprintf(ctx, "error resolving '%s': %s", *filename, strerror(errno));
+    (void)duk_throw(ctx);
+  }
+  if (!S_ISREG(path_stat.st_mode))
+  {
+    duk_push_sprintf(ctx, "'%s' is not a file", *filename);
+    (void)duk_throw(ctx);
+  }
+  return fopen(*filename, "r");
 }
 
 #define NOWRAP -1
 
-#define GETDATA(x) do {\
-  error=getdata(fp,&buf,&end,&cursize,(x));\
-  if(error && error!=GETDATAEND)\
-  {\
-      if(error==2000000)\
-        duk_push_string(ctx,"value too large to parse (>512mb)");\
-      else if(error==2000001)\
-        duk_push_string(ctx,"error realloc()");\
-      else\
-        duk_push_sprintf(ctx,"error opening or parsing'%s': %s",fn,strerror(errno));\
-      return duk_throw(ctx);\
-  }\
-} while (0)
+#define GETDATA(x)                                                                      \
+  do                                                                                    \
+  {                                                                                     \
+    error = getdata(fp, &buf, &end, &cursize, (x));                                     \
+    if (error && error != GETDATAEND)                                                   \
+    {                                                                                   \
+      if (error == 2000000)                                                             \
+        duk_push_string(ctx, "value too large to parse (>512mb)");                      \
+      else if (error == 2000001)                                                        \
+        duk_push_string(ctx, "error realloc()");                                        \
+      else                                                                              \
+        duk_push_sprintf(ctx, "error opening or parsing'%s': %s", fn, strerror(errno)); \
+      return duk_throw(ctx);                                                            \
+    }                                                                                   \
+  } while (0)
 
-#define GETVAL {}
-
-duk_ret_t duk_util_readcsv(duk_context *ctx) {
-  int wp=0, func_idx=-1, i=0;
-  FILE * fp;
-  const char *fn;
-  char *buf=(char*)NULL, *end=(char*)NULL;
-  size_t cursize=0;
-  int error=0,inval=1,inq=0,wrap=NOWRAP;
-  char *vs, *ve; /* value start and value end */   
-  fp=openFileWithCallback(ctx, &func_idx,&fn);
-/*
-  while (!error)
-  {
-    GETDATA(wrap);
-    GETVAL;
-  }
-
-
-
-  GETDATA(NOWRAP);
-  printf("buf=%p, end=%p\n",buf,end);
-  printf("cursize=%d, buf='%s'\n",(int)cursize, buf);
-  while (!error)
-  {
-    int cutoff=(int)(end-buf)-10;
-    GETDATA(cutoff);
-    printf("\nbuf=%p, end=%p\n",buf,end);
-    printf("cursize=%d, buf='%s'\n",(int)cursize, buf);
-  }
-
-  free(buf);
-*/  return 0;
-}
-
-
-duk_ret_t duk_util_readln(duk_context *ctx) {
-  int func_idx=-1;
-  FILE * fp;
-  const char *fn;
-  char * line = NULL;
+duk_ret_t duk_util_readln(duk_context *ctx)
+{
+  int func_idx = -1;
+  FILE *fp;
+  const char *filename;
+  char *line = NULL;
   size_t len = 0;
   ssize_t read;
-  
-  fp=openFileWithCallback(ctx, &func_idx,&fn);
-  
+
+  fp = openFileWithCallback(ctx, &func_idx, &filename);
+
   if (fp == NULL)
   {
-      const char *fn=duk_to_string(ctx,!func_idx);
-      duk_push_sprintf(ctx,"error opening '%s': %s",fn,strerror(errno));
-      return duk_throw(ctx);
+    const char *fn = duk_to_string(ctx, !func_idx);
+    duk_push_sprintf(ctx, "error opening '%s': %s", fn, strerror(errno));
+    return duk_throw(ctx);
   }
-  while ((read = getline(&line, &len, fp)) != -1) {
-      duk_dup(ctx,func_idx);
-      duk_push_this(ctx);
-      *(line+strlen(line)-1)='\0';
-      duk_push_string(ctx,line);
-      duk_call_method(ctx,1);
-      if(duk_is_boolean(ctx,-1) && duk_get_boolean(ctx,-1)==0)
-        break;
-      
-      duk_pop(ctx);
+  while ((read = getline(&line, &len, fp)) != -1)
+  {
+    duk_dup(ctx, func_idx);
+    duk_push_this(ctx);
+    *(line + strlen(line) - 1) = '\0';
+    duk_push_string(ctx, line);
+    duk_call_method(ctx, 1);
+    if (duk_is_boolean(ctx, -1) && duk_get_boolean(ctx, -1) == 0)
+      break;
+
+    duk_pop(ctx);
   }
 
   fclose(fp);
   if (line)
-      free(line);
+    free(line);
 
-  return(0);
+  return 0;
 }
 
+#define DUK_PUT(ctx, type, key, value, idx) \
+  {                                         \
+    duk_push_##type(ctx, value);            \
+    duk_put_prop_string(ctx, idx, key);     \
+  }
+/**
+ *  Filesystem stat
+ *  @param {string} The path name
+ *  @returns a javascript object of the following form:
+ *  stat: {
+ *    dev: { 
+ *      major: int, 
+ *      minor: int
+ *    },
+ *    inode: int,
+ *    type: int, // st_mode & S_IFMT
+ *    mode: int, // st_mode & 07777
+ *    num_links: int,
+ *    user_id: int,
+ *    group_id: int,
+ *    rdev: {
+ *      major: int,
+ *      minor: int
+ *    },
+ *    size: int,
+ *    block_size: int,
+ *    blocks: int,
+ *    last_access: Date,
+ *    last_modified: Date,
+ *    last_status_change: Date,
+ *  }
+ **/
+duk_ret_t duk_util_stat(duk_context *ctx)
+{
 
+  const char *path = duk_get_string(ctx, -1);
+  struct stat path_stat;
+  int err = stat(path, &path_stat);
+  if (err)
+  {
+    duk_push_sprintf(ctx, "error getting status '%s': %s", path, strerror(errno));
+    (void)duk_throw(ctx);
+  }
 
+  // stat
+  duk_push_object(ctx);
 
+  // dev
+  duk_push_object(ctx);
+  DUK_PUT(ctx, int, "major", major(path_stat.st_dev), -2);
+  DUK_PUT(ctx, int, "minor", major(path_stat.st_dev), -2);
+  duk_put_prop_string(ctx, -2, "dev");
+
+  DUK_PUT(ctx, int, "inode", path_stat.st_ino, -2);
+  DUK_PUT(ctx, int, "type", path_stat.st_mode &S_IFMT, -2);
+  DUK_PUT(ctx, int, "mode", path_stat.st_mode & 07777, -2);
+  DUK_PUT(ctx, int, "num_links", path_stat.st_nlink, -2);
+  DUK_PUT(ctx, int, "user_id", path_stat.st_uid, -2);
+  DUK_PUT(ctx, int, "group_id", path_stat.st_gid, -2);
+
+  // rdev
+  duk_push_object(ctx);
+  duk_push_int(ctx, major(path_stat.st_rdev));
+  duk_put_prop_string(ctx, -2, "major");
+  duk_push_int(ctx, minor(path_stat.st_rdev));
+  duk_put_prop_string(ctx, -2, "minor");
+  duk_put_prop_string(ctx, -2, "rdev");
+
+  DUK_PUT(ctx, int, "size", path_stat.st_size, -2);
+  DUK_PUT(ctx, int, "block_size", path_stat.st_blksize, -2);
+  DUK_PUT(ctx, int, "blocks", path_stat.st_blocks, -2);
+
+  long atime, mtime, ctime;
+#if __DARWIN_64_BIT_INO_T || !defined(_POSIX_C_SOURCE) || defined(_DARWIN_C_SOURCE)
+  atime = path_stat.st_atimespec.tv_sec * 1000;
+  mtime = path_stat.st_atimespec.tv_sec * 1000;
+  ctime = path_stat.st_atimespec.tv_sec * 1000;
+#else
+  atime = path_stat.atime;
+  mtime = path_stat.mtime;
+  ctime = path_stat.ctime;
+#endif
+
+  // last_access
+  (void)duk_get_global_string(ctx, "Date");
+  duk_push_number(ctx, atime);
+  duk_new(ctx, 1);
+  duk_put_prop_string(ctx, -2, "last_access");
+
+  // last_modified
+  (void)duk_get_global_string(ctx, "Date");
+  duk_push_number(ctx, mtime);
+  duk_new(ctx, 1);
+  duk_put_prop_string(ctx, -2, "last_modified");
+
+  // last_status_change
+  (void)duk_get_global_string(ctx, "Date");
+  duk_push_number(ctx, mtime);
+  duk_new(ctx, 1);
+  duk_put_prop_string(ctx, -2, "last_status_change");
+
+  // see duktape function interface
+  return 1;
+}
+
+/**
+ * Executes a command where the arguments are the arguments to execv.
+ * @returns a string with the stdout of the file
+ * Ex.
+ * var stdout = utils.exec("/bin/ls", "ls", "-1");
+ */
+duk_ret_t duk_util_exec(duk_context *ctx)
+{
+
+  // get variadic arguments and store in null terminated argument list
+  duk_idx_t nargs = duk_get_top(ctx);
+  char **args = NULL;
+  REMALLOC(args, (nargs + 1) * sizeof(char *));
+  for (int i = 0; i < nargs; i++)
+  {
+    args[i] = (char *)duk_get_string(ctx, i);
+  }
+  args[nargs] = NULL;
+
+  int pipes[2];
+  if (pipe(pipes) == -1)
+  {
+    duk_push_sprintf(ctx, "error creating pipes: %s", strerror(errno));
+    (void)duk_throw(ctx);
+  }
+
+  pid_t pid;
+  if ((pid = fork()) == -1)
+  {
+    duk_push_sprintf(ctx, "error forking: %s", strerror(errno));
+    (void)duk_throw(ctx);
+  }
+  else if (pid == 0)
+  {
+    // child process
+
+    // redirect to stdout
+    dup2(pipes[1], STDOUT_FILENO);
+    close(pipes[0]);
+    close(pipes[1]);
+    execv(args[0], args + 1);
+    fprintf(stderr, "error executing %s\n", args[1]);
+    exit(EXIT_FAILURE);
+  }
+  else
+  {
+    // read from stdin and buffer output
+    close(pipes[1]);
+    char *buf = NULL;
+    int size = BUFREADSZ;
+    REMALLOC(buf, size);
+    int nbytes = 0;
+    int nread = 0;
+    while ((nbytes = read(pipes[0], buf + nread, size - nread)) > 0)
+    {
+      size *= 2;
+      nread += nbytes;
+      REMALLOC(buf, size);
+    }
+    if (nbytes < 0)
+    {
+      duk_push_sprintf(ctx, "error reading stdout: %s", strerror(errno));
+      (void)duk_throw(ctx);
+    }
+    // push string to stack and exit
+    duk_push_string(ctx, buf);
+    free(buf);
+    free(args);
+    return 1;
+  }
+}
+
+duk_ret_t duk_util_readdir(duk_context *ctx)
+{
+}
 
 static const duk_function_list_entry utils_funcs[] = {
-    { "readln",         duk_util_readln, 2 /*nargs*/ },
-    { "readcsv",        duk_util_readcsv, 2 /*nargs*/ },
-    { NULL, NULL, 0 }
-};
+    {"readln", duk_util_readln, 2 /*nargs*/},
+    {"stat", duk_util_stat, 1},
+    {"exec", duk_util_exec, DUK_VARARGS},
+    {NULL, NULL, 0}};
+
+static const duk_number_list_entry file_types[] = {
+    {"SOCKET", S_IFSOCK},
+    {"LINK", S_IFLNK},
+    {"FILE", S_IFREG},
+    {"BLOCK_DEVICE", S_IFBLK},
+    {"DIRECTORY", S_IFDIR},
+    {"CHARACTER_DEVICE", S_IFCHR},
+    {"FIFO", S_IFIFO},
+    {NULL, 0.0}};
 
 static const duk_number_list_entry utils_consts[] = {
-    { NULL, 0.0 }
-};
+    {NULL, 0.0}};
 
-duk_ret_t dukopen_module(duk_context *ctx) {
-    duk_push_object(ctx);
-    duk_put_function_list(ctx, -1, utils_funcs);
-    duk_put_number_list(ctx, -1, utils_consts);
+duk_ret_t dukopen_module(duk_context *ctx)
+{
+  duk_push_object(ctx);
 
-    return 1;
+  // add stat constants
+  duk_push_object(ctx);
+  duk_put_number_list(ctx, -1, file_types);
+  duk_put_prop_string(ctx, -2, "file_types");
+
+  duk_put_function_list(ctx, -1, utils_funcs);
+  duk_put_number_list(ctx, -1, utils_consts);
+
+  return 1;
 }
-
