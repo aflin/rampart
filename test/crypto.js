703776a512fc381a427aca541eebf4abdcf3b6a9
Benjamin Flin <brf2117@columbia.edu>
2020-07-02 15:40:01 -0700

    added simple crypto test

--- test/crypto.js
+++ test/crypto.js
@@ -1,8 +1,16 @@
 var crypto = require("rpcrypto");
-var buffer = crypto.encrypt({
+var encBuffer = crypto.encrypt({
   key: "01234567890123456789012345678901",
   iv: "0123456789012345",
   cipher: "aes-256-cbc",
   buffer: new TextEncoder().encode("hello world"),
 });
-console.log(buffer.length);
+
+var decBuffer = crypto.decrypt({
+  key: "01234567890123456789012345678901",
+  iv: "0123456789012345",
+  cipher: "aes-256-cbc",
+  buffer: encBuffer,
+});
+
+console.log(new TextDecoder().decode(decBuffer));
