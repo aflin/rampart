c74e39e0b4403fd4b827313e33c0211d569f631a
root <root@bythe.kilomonkey.com>
2021-02-14 22:40:17 -0800

    don't attempt to compile rampart-robots.so on gcc<5.0

--- extern/extern.cmake
+++ extern/extern.cmake
@@ -14,7 +14,7 @@ add_subdirectory(${EXTERN_DIR}/libevhtp)
 
 add_subdirectory(${EXTERN_DIR}/tidy-html5)
 
-#add_subdirectory(${EXTERN_DIR}/robotstxt)
+if (NOT CMAKE_COMPILER_IS_GNUCC OR CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 5.0)
 
 include(cmake/ExternalProject.cmake)
 ExternalProject_Add( robotstxt
@@ -23,3 +23,5 @@ ExternalProject_Add( robotstxt
 	BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/extern/robotstxt" 
 	INSTALL_COMMAND cmake -E echo "Skipping install step."
 )
+
+endif()
