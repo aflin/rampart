7868c9c5c02137858f041d5628cee953f5c7a475
aflin <aflin@bigsink.com>
2021-02-26 16:02:24 -0800

    include example of normal regex

--- examples/web_server/modules/mymodulepath/hlshowreq.js
+++ examples/web_server/modules/mymodulepath/hlshowreq.js
@@ -17,6 +17,26 @@ rampart.globalize(Sql,["sandr"]);
 
 
 function showreq_callback(req){
+    // http://jsfiddle.net/KJQ9K/554/
+    function syntaxHighlight(json) {
+        json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+            var cls = 'number';
+            if (/^"/.test(match)) {
+                if (/:$/.test(match)) {
+                    cls = 'key';
+                } else {
+                    cls = 'string';
+                }
+            } else if (/true|false/.test(match)) {
+                cls = 'boolean';
+            } else if (/null/.test(match)) {
+                cls = 'null';
+            }
+            return '<span class="' + cls + '">' + match + '</span>';
+        });
+    }
+
     /* sandr is much faster than the above js regular expression engine */
     function sandrHighlight(json){
         /* first two lines are for objects, second two are for arrays */
