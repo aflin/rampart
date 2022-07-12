4bda85c906fa2dc99c741711099940c0cd50c0e5
aflin <aflin@bigsink.com>
2022-07-12 14:48:21 -0700

    version update

--- src/include/version.h
+++ src/include/version.h
@@ -6,7 +6,7 @@
 #define RAMPART_VERSION_MAJOR 0
 #define RAMPART_VERSION_MINOR 1
 #define RAMPART_VERSION_PATCH 1
-#define RAMPART_VERSION_PRERELEASE "-pre"
+#define RAMPART_VERSION_PRERELEASE ""
 
 void duk_rp_push_rampart_version(duk_context *ctx);
 
