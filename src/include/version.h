075fca5c82a702191e7c9ebb04821fa175baafd8
Aaron Flin <aaron@flin.org>
2023-04-06 07:30:47 -0700

    version 0.1.2

--- src/include/version.h
+++ src/include/version.h
@@ -5,7 +5,7 @@
 #define RAMPART_VERSION_H
 #define RAMPART_VERSION_MAJOR 0
 #define RAMPART_VERSION_MINOR 1
-#define RAMPART_VERSION_PATCH 1
+#define RAMPART_VERSION_PATCH 2
 #define RAMPART_VERSION_PRERELEASE ""
 
 void duk_rp_push_rampart_version(duk_context *ctx);
