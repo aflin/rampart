1ad1130d8fb9b62b6ad2272230d4ef4f95e48467
Aaron Flin <aaron@flin.org>
2024-10-18 11:59:24 -0700

    include build platform

--- src/version.c
+++ src/version.c
@@ -17,10 +17,16 @@ void duk_rp_push_rampart_version(duk_context *ctx)
         duk_pop(ctx);
         duk_push_object(ctx);
     }
+
+    duk_push_string(ctx, RAMPART_BUILD_PLATFORM);
+    duk_put_prop_string(ctx, -2, "buildPlatform");
+
     duk_push_sprintf(ctx, "%d.%d.%d%s_%dbit+%s", RAMPART_VERSION_MAJOR, RAMPART_VERSION_MINOR, RAMPART_VERSION_PATCH, RAMPART_VERSION_PRERELEASE, bits, RAMPART_VERSION_TIMESTAMP);
     duk_put_prop_string(ctx, -2, "versionBuild");
+
     duk_push_sprintf(ctx, "%d.%d.%d%s", RAMPART_VERSION_MAJOR, RAMPART_VERSION_MINOR, RAMPART_VERSION_PATCH, RAMPART_VERSION_PRERELEASE);
     duk_put_prop_string(ctx, -2, "version");
+
     duk_push_number(ctx, (double) RAMPART_VERSION_MAJOR*10000 +  RAMPART_VERSION_MINOR*100 + RAMPART_VERSION_PATCH);
     duk_put_prop_string(ctx, -2, "versionNumber");
 
