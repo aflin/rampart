5622140a178194042a28e413fe04183d40dbeedf
Aaron Flin <aaron@flin.org>
2026-03-05 22:04:18 -0800

    server return object: noAcceptRanges

--- src/duktape/modules/rampart-server.c
+++ src/duktape/modules/rampart-server.c
@@ -1964,6 +1964,12 @@ static int rp_evbuffer_add_file(struct evbuffer *outbuf, int fd, ev_off_t offset
     // if( !rp_using_ssl || length-offset > 5242880 )
     //    return evbuffer_add_file(outbuf, fd, offset, length);
 
+    // Not sure, what the speed issue above was (many years have past)
+    // but we probably don't want to risk putting gigabytes into memory
+    // --ajf 2026-03-05
+    if(length-offset > 5242880 )
+        return evbuffer_add_file(outbuf, fd, offset, length);
+
     {
         size_t off=0;
         ssize_t nbytes=0;
@@ -2080,8 +2086,11 @@ static char *cachedir = ".gzipcache/";
     Send a file, whole or partial with range if requested
       haveCT - non-zero if content-type is already set
       filestat - if already called stat, include here, otherwise set to NULL
+      sendfile_flags - bitmask: SENDFILE_NO_RANGE_CAP, SENDFILE_NO_ACCEPT_RANGES
 */
-static void rp_sendfile(evhtp_request_t *req, char *fn, int haveCT, struct stat *filestat, int is_fileserver, int noRangeCap)
+#define SENDFILE_NO_RANGE_CAP      0x01
+#define SENDFILE_NO_ACCEPT_RANGES  0x02
+static void rp_sendfile(evhtp_request_t *req, char *fn, int haveCT, struct stat *filestat, int is_fileserver, uint16_t sendfile_flags)
 {
     int fd = -1;
     ev_off_t beg = 0, len = -1;
@@ -2236,7 +2245,8 @@ static void rp_sendfile(evhtp_request_t *req, char *fn, int haveCT, struct stat 
 
 
     /* http range - give back partial file, set 206 */
-    evhtp_headers_add_header(req->headers_out, evhtp_header_new("Accept-Ranges", "bytes", 0, 0));
+    if (!(sendfile_flags & SENDFILE_NO_ACCEPT_RANGES))
+        evhtp_headers_add_header(req->headers_out, evhtp_header_new("Accept-Ranges", "bytes", 0, 0));
     range=evhtp_kv_find(req->headers_in,"Range");
     if (range && !strncasecmp("bytes=", range, 6))
     {
@@ -2267,7 +2277,7 @@ static void rp_sendfile(evhtp_request_t *req, char *fn, int haveCT, struct stat 
                 if (endval && endval > beg)
                     len = (endval - beg) +1;
             }
-            else if (!noRangeCap && filesize - beg > (ev_off_t)default_range_bytes)
+            else if (!(sendfile_flags & SENDFILE_NO_RANGE_CAP) && filesize - beg > (ev_off_t)default_range_bytes)
             {   // don't send whole file, just a small chunk.
                 len = default_range_bytes;
                 endval = -1 + beg + len;
@@ -2949,11 +2959,17 @@ static evhtp_res obj_to_buffer(DHS *dhs)
     }
     duk_pop(ctx);
 
-    int noRangeCap = 0;
+    uint16_t sendfile_flags = 0;
     if (duk_get_prop_string(ctx, -1, "noRangeCap"))
     {
-        if (duk_is_boolean(ctx, -1))
-            noRangeCap = duk_get_boolean(ctx, -1);
+        if (duk_is_boolean(ctx, -1) && duk_get_boolean(ctx, -1))
+            sendfile_flags |= SENDFILE_NO_RANGE_CAP;
+    }
+    duk_pop(ctx);
+    if (duk_get_prop_string(ctx, -1, "noAcceptRanges"))
+    {
+        if (duk_is_boolean(ctx, -1) && duk_get_boolean(ctx, -1))
+            sendfile_flags |= SENDFILE_NO_ACCEPT_RANGES;
     }
     duk_pop(ctx);
 
@@ -2965,7 +2981,8 @@ static evhtp_res obj_to_buffer(DHS *dhs)
         if( !strcmp(
             key, "headers") || !strcmp(key, "chunk") ||
             !strcmp(key, "chunkDelay") ||!strcmp(key, "status") ||
-            !strcmp(key, "compress") || !strcmp(key, "noRangeCap")
+            !strcmp(key, "compress") || !strcmp(key, "noRangeCap") ||
+            !strcmp(key, "noAcceptRanges")
         ){
             duk_pop_2(ctx);
             continue;
@@ -2998,7 +3015,7 @@ static evhtp_res obj_to_buffer(DHS *dhs)
                 }
                 else
                 {
-                    rp_sendfile(dhs->req, (char *)d+1, 1, NULL, 0, noRangeCap);
+                    rp_sendfile(dhs->req, (char *)d+1, 1, NULL, 0, sendfile_flags);
                     return (0);
                 }
             }
